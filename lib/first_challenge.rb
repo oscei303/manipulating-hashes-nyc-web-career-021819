3478601f993fc5894cf9afb91e5beff357658ca3
OSCAR R <oscr.rss@gmail.com>
2019-02-15 04:06:53 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -12,11 +13,6 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|x| x == "strawberry" }
+contacts
 end
-
